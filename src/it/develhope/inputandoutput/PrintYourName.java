7f4cb0b52ce2b37ffc78e1dcab89e5cc30afac90
Mirco Forcella <forcella1996@gmail.com>
2023-01-22 00:46:20 +0100

    Initial commit

---
+++ src/it/develhope/inputandoutput/PrintYourName.java
@@ -0,0 +1,10 @@
+package it.develhope.inputandoutput;
+
+public class PrintYourName {
+    public static void main(String[] args) {
+        /**
+         * io tilto male.
+         */
+        System.out.print("Mirco");
+    }
+}
